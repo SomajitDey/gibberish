86588530cefff4019f18fa0c32fa3b334de26282
Somajit <dey.somajit@gmail.com>
2021-03-21 19:01:09 +0530

    broke it

--- gibberish_rc.bash
+++ gibberish_rc.bash
@@ -3,7 +3,6 @@ export GIBBERISH_DIR="${GIBBERISH_DIR:="${HOME}/.gibberish"}"
 GIBBERISH_filesys(){
   export incoming_dir="${GIBBERISH_DIR}/incoming"
   export outgoing_dir="${GIBBERISH_DIR}/outgoing"
-  export git_dir="${GIBBERISH_DIR}/.git"
   export iofile="io.txt"
   export incoming="${GIBBERISH_DIR}/incoming.fifo"
   export outgoing="${GIBBERISH_DIR}/outgoing.txt"
@@ -61,24 +60,24 @@ GIBBERISH_read(){
 # We could have used tail -n+1 -F "${incoming}" 2>/dev/null;
 # With incoming being a text file instead of fifo
 # tail -f however would be polling the file, hence busy wait, which is undesirable
-  while cat "${incoming}"; do : ; done
+  while cat "${incoming}"; do : ; done # : implies no-op
 }
 export -f GIBBERISH_read
 
 GIBBERISH_prelaunch(){
   GIBBERISH_filesys
-  (
+
   cd "${incoming_dir}"
   git pull --ff-only --no-verify --quiet origin "${fetch_branch}" || \
-    (echo 'Pull failed' >&2 ; exit) 
+    { echo 'Pull failed' >&2 ; exit;}
   until git tag last_read &>/dev/null; do git tag -d last_read &>/dev/null; done
-
-  mkfifo "${incoming}" || (echo 'Pipe exists: May be another session running' >&2 ; exit)
+  cd "${OLDPWD}"
+  
+  mkfifo "${incoming}" || { echo 'Pipe exists: May be another session running' >&2 ; exit;}
   touch "${push_lock}"
   touch "${commit_lock}"
   touch "${checkout_lock}"
   touch "${write_lock}"
-  )
 }
 export -f GIBBERISH_prelaunch
 
